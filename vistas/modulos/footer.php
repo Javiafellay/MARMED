31ec467c0b564c4aef1b226c458ad23a55d41aaa
Javier <javiafellay96@gmail.com>
2020-09-08 04:03:05 -0400

    Primera Modificacion

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2020 <a href="https://www.tutorialesatualcance.com" target="_blank">Javier Martinez Guanez</a>.</strong>
+
+	Todos los derechos reservados.
+
+
+</footer>
